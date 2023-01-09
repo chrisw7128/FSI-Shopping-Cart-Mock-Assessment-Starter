9c18209a40df27c0ab1aba3dbb0467a69945fc8e
Chris Wehner <chrisw7128@gmail.com>
2023-01-09 14:00:18 -0800

    second commit

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,21 @@
 let increase = document.getElementById("quantity-up");
 let decrease = document.getElementById("quantity-down");
 
-let product_quantity = 0;
+let product_quantity = 1;
+
+let total_quantity = document.querySelector(".total-quantity");
 
 increase.addEventListener("click", function () {
   console.log("plus one");
   product_quantity += 1;
   console.log(product_quantity);
+  total_quantity.textContent = `Quantity: ${product_quantity}`;
 });
 decrease.addEventListener("click", function () {
-  console.log("minus one");
-  product_quantity -= 1;
-  console.log(product_quantity);
+  if (product_quantity > 0) {
+    console.log("minus one");
+    product_quantity -= 1;
+    console.log(product_quantity);
+  }
+  total_quantity.textContent = `Quantity: ${product_quantity}`;
 });
